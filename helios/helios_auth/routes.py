a03123f0ea952307e7193e7f5801ad7d342174ec
Tobal <jaramillo.cristobal.a@gmail.com>
2022-04-05 19:43:25 -0400

    CAS Login

--- helios/helios_auth/routes.py
+++ helios/helios_auth/routes.py
@@ -1,7 +1,9 @@
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, redirect, session
 from helios import app
 from werkzeug.security import check_password_hash
 from helios.helios_auth.models import User
+from helios import cas_client
+from helios import config
 
 import datetime
 import jwt
@@ -26,3 +28,30 @@ def login_user():
 
     else:
         return make_response({'message': 'Usuario o contraseñas incorrectos'}, 401)
+
+
+
+@app.route('/CASLogin', methods=['GET', 'POST'])
+def CASLogin():
+
+    if 'username' in session:
+        # Already logged in
+        return redirect(config['URL']['front'] + "/cabina/2", code=302)
+
+    ticket = request.args.get('ticket')
+    print("ticket", ticket)
+    if not ticket:
+        # No ticket, the request come from end user, send to CAS login
+        cas_login_url = cas_client.get_login_url()
+        
+        return redirect(cas_login_url)
+
+    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
+    if not user:
+        return make_response({'message': 'ERROR'}, 401)
+    else:  # Login successfully, redirect according `next` query parameter.
+        session['username'] = user
+        return redirect(config['URL']['front'] + "/cabina/2", code=302)
+
+
+
