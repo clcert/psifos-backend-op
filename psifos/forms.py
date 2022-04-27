4ece97341e9750ad835f0b36c530e0b612259289
Raúl De La Fuente <raul.delafuente@ing.uchile.cl>
2022-04-27 15:39:54 -0400

    fixed bug forms and psifos_auth.utils

--- psifos/forms.py
+++ psifos/forms.py
@@ -14,7 +14,7 @@ class ElectionForm(Form):
 
     max_weight = IntegerField('max_weight', validators=[DataRequired()])
 
-    obscure_voter_names = BooleanField('use_voter_aliases')
+    obscure_voter_names = BooleanField('obscure_voter_names')
     randomize_answer_order = BooleanField('randomize_answer_order')
     private_p = BooleanField('private_p')
     normalization = BooleanField('normalization')
