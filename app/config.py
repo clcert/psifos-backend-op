1b2c73a6964da0048798935a76b8cfb97b11be57
Tobal <jaramillo.cristobal.a@gmail.com>
2022-08-04 20:39:58 -0400

    adding cors

--- app/config.py
+++ app/config.py
@@ -4,12 +4,18 @@ from pydantic import BaseSettings
 
 # Retrieve enviroment variables from .env file
 env = configparser.ConfigParser()
-env.read('.env')
+env.read(".env")
+
 
 class Settings(BaseSettings):
-    CORS_HEADERS: str = 'Content-Type'
+    CORS_HEADERS: str = "Content-Type"
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = True
-    SECRET_KEY: str='Th1s1ss3cr3t'
+    SECRET_KEY: str = "Th1s1ss3cr3t"
     JWT_ACCESS_TOKEN_EXPIRES: timedelta = timedelta(hours=1000)
+    ORIGINS = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from datetime import timedelta
 from fastapi import FastAPI
 
+from app.config import Settings
+
 from .database import Base, engine
 from .psifos.routes import api_router
+from .psifos_auth.routes import auth_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Settings.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
 app.include_router(api_router)
+app.include_router(auth_router)
