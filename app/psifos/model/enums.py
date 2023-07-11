3092c8172e9b11756d3860e68a1709f8007873bd
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-07-11 18:58:44 -0400

    adding new step to computing tally

--- app/psifos/model/enums.py
+++ app/psifos/model/enums.py
@@ -6,27 +6,30 @@ Enums for Psifos model.
 
 import enum
 
+
 class ElectionTypeEnum(str, enum.Enum):
     query = "Query"
     election = "Election"
+
 
 class ElectionStatusEnum(str, enum.Enum):
     setting_up = "Setting up"
     started = "Started"
     ended = "Ended"
+    computing_tally = "Computing Tally"
     tally_computed = "Tally computed"
     decryptions_uploaded = "Decryptions uploaded"
     decryptions_combined = "Decryptions combined"
     results_released = "Results released"
 
-class ElectionEventEnum(str, enum.Enum):
 
+class ElectionEventEnum(str, enum.Enum):
     @classmethod
     def has_member_key(cls, key):
         return key in cls.__members__.values()
 
-class ElectionPublicEventEnum(ElectionEventEnum):
 
+class ElectionPublicEventEnum(ElectionEventEnum):
     VOTER_FILE_UPLOADED = "voter_file_uploaded"
     ELECTORAL_ROLL_MODIFIED = "electoral_roll_modified"
     TRUSTEE_CREATED = "trustee_created"
@@ -36,6 +39,7 @@ class ElectionPublicEventEnum(ElectionEventEnum):
     TALLY_COMPUTED = "tally_computed"
     DECRYPTION_RECIEVED = "decryption_recieved"
     DECRYPTIONS_COMBINED = "decryptions_combined"
+
 
 class ElectionAdminEventEnum(ElectionEventEnum):
     VOTER_LOGIN = "voter_login"
