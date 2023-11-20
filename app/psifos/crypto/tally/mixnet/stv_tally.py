5ba1e26fd9536d6925ed1ae30b259456ef8df258
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2023-11-20 14:47:29 -0300

    feat: stv questions supports null, blank and invalid votes
    All valid and formal ballots is consider in stv  calculus
    All the blank and null votes are counted independently.
    All invalid vote is counted as a null vote.
    If we have n candidates and the answer supports blank and null answers, the results is an array of n+2 elements.
    For the element on the index i, considering the first n elements like A, if the candidate i was elected A[i] = 1, otherwise A[i] = 0.

--- app/psifos/crypto/tally/mixnet/stv_tally.py
+++ app/psifos/crypto/tally/mixnet/stv_tally.py
@@ -1,7 +1,7 @@
 """
 Mixnet tally for STV psifos questions.
 
-14-09-2023
+16-11-2023
 """
 
 from app.psifos.crypto.tally.mixnet.tally import MixnetTally
@@ -9,15 +9,58 @@ from stvpoll.scottish_stv import ScottishSTV
 
 class STVTally(MixnetTally):
     def count_votes(self, votes, total_closed_options):
-        # STV questions doesn't include blank and null options
-        control_vote = votes[0]
-        if (total_closed_options == len(control_vote)):
-            poll = ScottishSTV(seats=1, candidates=sorted(votes[0]))
+        # All ballots have the same length
+        formal_options = len(votes[0])
+        null_index = formal_options + 1
+        blank_index = formal_options
+        null_id = total_closed_options + 1
+        blank_id = total_closed_options
+        null_vote = [null_id]*formal_options
+        blank_vote = [blank_id]*formal_options
+        
+        # Some aux functions
+        def is_blank_vote(vote):
+            return vote == blank_vote
+        def is_null_vote(vote):
+            return vote == null_vote
+        def is_invalid_vote(vote):
+            for el in vote:
+                closed_options = list(range(formal_options)) + [null_id, blank_id]
+                isValid = el in closed_options
+                if vote.count(el) > 1 or not isValid:
+                    return True
+            return False
 
-            # Add each vote to the poll
-            for vote in votes:
-                poll.add_ballot(vote, 1)
+        # Analyzes each vote
+        poll = ScottishSTV(seats=1, candidates=list(range(formal_options)))
+        blank_count = 0
+        null_count = 0
+        for vote in votes:
+            if is_blank_vote(vote):
+                blank_count += 1
+            elif is_null_vote(vote) or is_invalid_vote(vote):
+                null_count += 1
+            else:
+                candidates = list(range(formal_options))
+                final_vote = []
+                for candidate in vote:
+                    if candidate in candidates:
+                        final_vote += [candidate]
+                    else:
+                        break
 
-            # Calculates the result
-            result = list(poll.calculate())
-            return result
+                ## lo agrego al calculo de stv
+                poll.add_ballot(final_vote, 1)
+
+        # Calculates the stv result
+        stv_result = list(poll.calculate())
+
+        # Forms the final result 
+        results = [0] * total_closed_options
+        for winner_id in stv_result:
+            results[winner_id] = 1
+        if total_closed_options != formal_options:
+            results[null_index] = null_count
+            results[blank_index] = blank_count
+
+        return results
